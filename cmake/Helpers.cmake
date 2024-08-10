367ba4325ff7c4c387e155df582df117c64cd5ac
SunRain <41245110@qq.com>
2024-08-11 00:28:09 +0800

    support shared/static link of qwlroots

--- cmake/Helpers.cmake
+++ cmake/Helpers.cmake
@@ -1,4 +1,4 @@
-function(add_pkgconfig_module name library include_dir depends)
+function(add_pkgconfig_module name library include_dir depends description)
     # For cmake
     include(CMakePackageConfigHelpers)
     configure_file(${PROJECT_SOURCE_DIR}/src/cmake/pkgconfig.pc.in
